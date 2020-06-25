73452da6e1b7b01833cf54945930b7e9d87c5303
Tarxio <tarxil@icloud.com>
2020-06-26 02:03:51 +0300

    Header&Footer

---
+++ blocks/footer.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+
+<footer class="blog-footer">
+    <p>Blog template built for TIO by TarxovI.O.</p>
+    <p>
+        <a href="#">Back to top</a>
+    </p>
+</footer>
+
--- index.php
+++ index.php
@@ -1,1 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <title>Готовый WEB-сайт </title>
+</head>
+<body>
+
+    <?php require "blocks/header.php" ?>
+
+<table border="1">
+    <tr>
+        <th>Ячейка 1</th>
+        <th>Ячейка 2</th>
+    </tr>
+    <tr>
+        <td>Ячейка 3</td>
+        <td>Ячейка 4</td>
+    </tr>
+</table>
+
+    <?php require "blocks/footer.php" ?>
+
+</body>
+</html>
